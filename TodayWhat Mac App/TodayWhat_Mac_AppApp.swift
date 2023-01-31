df8ab36fd7df066b26f8527974ac439839a1995d
baegteun <baegteun@gmail.com>
2023-02-01 00:20:48 +0900

    :sparkles: :: MacOS Target

---
+++ TodayWhat Mac App/TodayWhat_Mac_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayWhat_Mac_AppApp.swift
+//  TodayWhat Mac App
+//
+//  Created by 최형우 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct TodayWhat_Mac_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
