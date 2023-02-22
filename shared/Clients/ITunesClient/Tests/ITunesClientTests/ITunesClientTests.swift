7cf101a4b2031044f31bbf4ce00c7d6dcefbbfb5
baegteun <baegteun@gmail.com>
2023-02-22 19:57:49 +0900

    :sparkles: :: ITunesClient

---
+++ shared/Clients/ITunesClient/Tests/ITunesClientTests/ITunesClientTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ITunesClient
+
+final class ITunesClientTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ITunesClient().text, "Hello, World!")
+    }
+}
