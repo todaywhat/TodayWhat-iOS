2b93388074a63516f3db0f934834d876572d572e
baegteun <baegteun@gmail.com>
2023-03-29 10:25:10 +0900

    :sparkles: :: 알러지 포함여부 정상화

--- TodayWhatWidget/MealWidgetEntryView.swift
+++ TodayWhatWidget/MealWidgetEntryView.swift
@@ -89,7 +89,7 @@ private struct SmallMealWidgetView: View {
 
     private func isMealContainsAllergy(meal: String) -> Bool {
         entry.allergyList
-            .first { meal.contains($0.number) } != nil
+            .first { meal.contains("(\($0.number)") || meal.contains(".\($0.number)") } != nil
     }
 }
 
@@ -151,7 +151,7 @@ private struct MediumMealWidgetView: View {
 
     private func isMealContainsAllergy(meal: String) -> Bool {
         entry.allergyList
-            .first { meal.contains($0.number) } != nil
+            .first { meal.contains("(\($0.number)") || meal.contains(".\($0.number)") } != nil
     }
 }
 
@@ -223,7 +223,7 @@ private struct LargeMealWidgetView: View {
 
     private func isMealContainsAllergy(meal: String) -> Bool {
         entry.allergyList
-            .first { meal.contains($0.number) } != nil
+            .first { meal.contains("(\($0.number)") || meal.contains(".\($0.number)") } != nil
     }
 }
 
