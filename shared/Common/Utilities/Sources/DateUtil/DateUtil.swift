027dc89f780bf1960f2c6816013af8896f6be5e6
baegteun <baegteun@gmail.com>
2023-04-05 08:43:52 +0900

    :sparkles: :: 시간표 위젯 추가

--- shared/Common/Utilities/Sources/DateUtil/DateUtil.swift
+++ shared/Common/Utilities/Sources/DateUtil/DateUtil.swift
@@ -22,6 +22,19 @@ public extension Date {
         return Calendar.current.component(.weekday, from: self)
     }
 
+    var weekdayString: String {
+        switch weekday {
+        case 1: return "일요일"
+        case 2: return "월요일"
+        case 3: return "화요일"
+        case 4: return "수요일"
+        case 5: return "목요일"
+        case 6: return "금요일"
+        case 7: return "토요일"
+        default: return ""
+        }
+    }
+
     func adding(by component: Calendar.Component, value: Int) -> Date {
         return Calendar.current.date(byAdding: component, value: value, to: self) ?? self
     }
