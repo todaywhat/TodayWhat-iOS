7d1dc5bea03ac75c7629b4a05fd2836a78aed398
baegteun <baegteun@gmail.com>
2023-04-12 17:09:26 +0900

    :green_heart: :: plist location

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/baekteun/TodayWhat-XCConfig.git
-mv TodayWhat-XCConfig/ .
+mv todaywhat-xcconfig/XCConfig .
 
-mv TodayWhat-XCConfig/GoogleService-Info.plist ./TodayWhat 
+mv TodayWhat-XCConfig/GoogleService-Info.plist ./TodayWhat/GoogleService-Info.plist 
 
