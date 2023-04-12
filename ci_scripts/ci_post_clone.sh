b34095de22fbbb3f36f0159f1d0f68d00cff7b09
baegteun <baegteun@gmail.com>
2023-04-12 17:31:21 +0900

    :green_heart: :: Path Error

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/baekteun/TodayWhat-XCConfig.git
-mv todaywhat-xcconfig/XCConfig .
 cd ../
+mv ./ci_scripts/Todaywhat-xcconfig/XCConfig .
 mv ./ci_scripts/TodayWhat-XCConfig/GoogleService-Info.plist ./TodayWhat/GoogleService-Info.plist
