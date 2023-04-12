768f0656f3cf3b963fec3e14e94186f61aaff410
baegteun <baegteun@gmail.com>
2023-04-12 16:52:43 +0900

    :green_heart: :: GoogleService Info

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
 git clone https://github.com/baekteun/TodayWhat-XCConfig.git
 mv TodayWhat-XCConfig/XCConfig/ .
+
+mv TodayWhat-XCConfig/GoogleService-Info.plist ./TodayWhat/GoogleService-Info.plist 
+
