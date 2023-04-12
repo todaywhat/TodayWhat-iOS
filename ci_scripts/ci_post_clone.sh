30bcaf2b0bad920520e5e3d6f806dad419491e8a
baegteun <baegteun@gmail.com>
2023-04-12 17:26:07 +0900

    :green_heart: :: plist path location to root

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/baekteun/TodayWhat-XCConfig.git
 mv todaywhat-xcconfig/XCConfig .
-
+cd ../
+mv ./ci_scripts/TodayWhat-XCConfig/GoogleService-Info.plist ./TodayWhat/GoogleService-Info.plist
--- ci_scripts/ci_pre_xcodebuild.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-git clone https://github.com/baekteun/TodayWhat-XCConfig
-
-ls
-mv TodayWhat-XCConfig/GoogleService-Info.plist ./TodayWhat/GoogleService-Info.plist 
-cd ../
-ls
