5d802875dbd99ecbf65ba6b5a356e91059a55edb
baegteun <baegteun@gmail.com>
2023-06-28 22:47:26 +0900

    :bug: :: 공지 리스트가 단일만 보이는 현상, 정렬 역순으로 변경

--- TodayWhat/Sources/FeaturesModule/Sources/RootFeature/RootView.swift
+++ TodayWhat/Sources/FeaturesModule/Sources/RootFeature/RootView.swift
@@ -15,24 +15,18 @@ public struct RootView: View {
         SwitchStore(store) {
             switch $0 {
             case .splashCore:
-                VStack {
-                    CaseLet(state: /RootCore.State.splashCore, action: RootCore.Action.splashCore) { store in
-                        SplashView(store: store)
-                    }
+                CaseLet(state: /RootCore.State.splashCore, action: RootCore.Action.splashCore) { store in
+                    SplashView(store: store)
                 }
 
             case .schoolSettingCore:
-                VStack {
-                    CaseLet(state: /RootCore.State.schoolSettingCore, action: RootCore.Action.schoolSettingCore) { store in
-                        SchoolSettingView(store: store)
-                    }
+                CaseLet(state: /RootCore.State.schoolSettingCore, action: RootCore.Action.schoolSettingCore) { store in
+                    SchoolSettingView(store: store)
                 }
 
             case .mainCore:
-                VStack {
-                    CaseLet(state: /RootCore.State.mainCore, action: RootCore.Action.mainCore) { store in
-                        MainView(store: store)
-                    }
+                CaseLet(state: /RootCore.State.mainCore, action: RootCore.Action.mainCore) { store in
+                    MainView(store: store)
                 }
             }
         }
