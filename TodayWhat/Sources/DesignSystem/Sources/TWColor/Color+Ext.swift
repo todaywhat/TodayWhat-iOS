1e1fa51dbfcf8bb5e0b36bd06d7084cad59cb044
baegteun <baegteun@gmail.com>
2023-10-19 14:21:11 +0900

    :bento: :: [#26] DesignSystem / Redesign color system 에셋 추가

--- TodayWhat/Sources/DesignSystem/Sources/TWColor/Color+Ext.swift
+++ TodayWhat/Sources/DesignSystem/Sources/TWColor/Color+Ext.swift
@@ -1,12 +1,26 @@
 import SwiftUI
 
 public extension Color {
+    @available(*, unavailable, message: "This is deprecated, use Neutral colors instead.")
     static let extraPrimary = Color("Primary", bundle: .module)
+    @available(*, unavailable, message: "This is deprecated, use Neutral colors instead.")
     static let veryLightGray = Color("VeryLightGray", bundle: .module)
+    @available(*, unavailable, message: "This is deprecated, use Neutral colors instead.")
+    static let lightGray = Color("LightGray", bundle: .module)
+    @available(*, unavailable, message: "This is deprecated, use Neutral colors instead.")
+    static let extraGray = Color("Gray", bundle: .module)
+    @available(*, unavailable, message: "This is deprecated, use Neutral colors instead.")
+    static let darkGray = Color("DarkGray", bundle: .module)
+    @available(*, unavailable, message: "This is deprecated, use Neutral colors instead.")
+    static let background = Color("BG", bundle: .module)
+
     static let extraBlack = Color("Black", bundle: .module)
     static let extraWhite = Color("White", bundle: .module)
-    static let lightGray = Color("LightGray", bundle: .module)
-    static let extraGray = Color("Gray", bundle: .module)
-    static let darkGray = Color("DarkGray", bundle: .module)
-    static let background = Color("BG", bundle: .module)
+    static let n00 = Color("N00", bundle: .module)
+    static let n10 = Color("N10", bundle: .module)
+    static let n20 = Color("N20", bundle: .module)
+    static let n30 = Color("N30", bundle: .module)
+    static let n40 = Color("N40", bundle: .module)
+    static let lightBox = Color("LightBox", bundle: .module)
+    static let point = Color("Point", bundle: .module)
 }
