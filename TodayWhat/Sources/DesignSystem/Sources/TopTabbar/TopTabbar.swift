93cc75a054d7352ae4ce72ee5a83ed3fe5e9c262
baegteun <baegteun@gmail.com>
2023-11-19 00:15:52 +0900

    :lipstick: :: [#41] Tabbar layout 수정

--- TodayWhat/Sources/DesignSystem/Sources/TopTabbar/TopTabbar.swift
+++ TodayWhat/Sources/DesignSystem/Sources/TopTabbar/TopTabbar.swift
@@ -15,46 +15,39 @@ public struct TopTabbarView: View {
     }
 
     public var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                ForEach(items.indices, id: \.self) { index in
-                    Button {
-                        withAnimation {
-                            currentTab = index
-                        }
-                    } label: {
-                        VStack {
-                            Text(items[index])
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundColor(currentTab == index ? .extraBlack : .unselectedPrimary)
-
-                            if currentTab == index {
-                                RoundedRectangle(cornerRadius: 17)
-                                    .fill(Color.textPrimary)
-                                    .frame(height: 2)
-                                    .matchedGeometryEffect(id: "TAB", in: tabAnimation, properties: .position)
-                            } else {
-                                Color.clear
-                                    .frame(height: 2)
-                            }
-                        }
-                        .frame(maxWidth: .infinity)
-                        .background {
-                            Color.extraWhite
-                        }
-                        .padding(.horizontal, 16)
+        HStack {
+            ForEach(items.indices, id: \.self) { index in
+                Button {
+                    withAnimation {
+                        currentTab = index
                     }
-
+                } label: {
+                    VStack {
+                        Text(items[index])
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(currentTab == index ? .extraBlack : .unselectedPrimary)
+                        
+                        if currentTab == index {
+                            RoundedRectangle(cornerRadius: 17)
+                                .fill(Color.textPrimary)
+                                .frame(height: 2)
+                                .matchedGeometryEffect(id: "TAB", in: tabAnimation, properties: .position)
+                                .offset(y: 0.5)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background {
+                        Color.extraWhite
+                    }
+                    .padding(.horizontal, 16)
                 }
             }
-
-            LinearGradient(
-                colors: [.extraWhite, .unselectedSecondary.opacity(0.08)],
-                startPoint: .bottom,
-                endPoint: .top
-            )
-            .frame(height: 8)
-            .opacity(0.8)
+        }
+        .background(alignment: .bottom) {
+            Rectangle()
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.unselectedSecondary)
+                .frame(height: 1)
         }
     }
 }
