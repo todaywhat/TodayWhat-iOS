7b6f4424886689bdda4f21d1df90a65b175a36e2
baegteun <baegteun@gmail.com>
2024-01-11 23:23:32 +0900

    :arrow_up: :: [#19] TCA 0.59.0 -> 1.6.0

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "TodayWhatPackage",
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "0.59.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.6.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", exact: "5.0.0"),
         .package(url: "https://github.com/groue/GRDB.swift.git", exact: "6.16.0"),
         .package(url: "https://github.com/sindresorhus/LaunchAtLogin.git", exact: "5.0.0"),
