331429fc2107bff19f5aeca8f96152851f1e91e5
baegteun <baegteun@gmail.com>
2024-01-16 20:04:36 +0900

    :truck: :: [#66] UserDefaultsKeys 위치 UserDefaultsClient -> FoundationUtil

--- Projects/Shared/FoundationUtil/Sources/UserDefaults+app.swift
+++ Projects/Shared/FoundationUtil/Sources/UserDefaults+app.swift
@@ -1,6 +1,20 @@
 import ConstantUtil
 import Foundation
 
+public enum UserDefaultsKeys: String {
+    case schoolType = "SCHOOL-TYPE"
+    case orgCode = "ORG-CODE"
+    case schoolCode = "SCHOOL-CODE"
+    case school = "SCHOOL"
+    case grade = "GRADE"
+    case `class` = "CLASS"
+    case major = "MAJOR"
+    case isSkipWeekend = "IS-SKIP-WEEKEND"
+    case isSkipAfterDinner = "IS-SKIP-AFTER-DINNER"
+    case isOnModifiedTimeTable = "IS-ON-MODIFIED-TIME-TABLE"
+    case widgetCount = "WIDGET-COUNT"
+}
+
 public extension UserDefaults {
     static var app: UserDefaults {
         let group = AppGroup.group.rawValue
