3ea404dd076a530ee194c047e96d03d04fd30093
baegteun <baegteun@gmail.com>
2024-01-16 23:22:39 +0900

    :sparkles: :: [#73] BaseFeature / PageShowedEventLog

---
+++ Projects/Feature/BaseFeature/Sources/PageShowedEventLog.swift
@@ -0,0 +1,12 @@
+import TWLog
+
+public struct PageShowedEventLog: EventLog {
+    public let name: String = "page_showed"
+    public let params: [String: Any]
+
+    public init(pageName: String) {
+        self.params = EventLogParameterBuilder()
+            .set(key: "page_name", value: pageName)
+            .build()
+    }
+}
