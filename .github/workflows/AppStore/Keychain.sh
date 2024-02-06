8309d248442cafcb64a43bade058d85ffdf67486
baegteun <baegteun@gmail.com>
2024-02-07 01:32:56 +0900

    :pencil2: :: 오탈자 수정

--- .github/workflows/AppStore/Keychain.sh
+++ .github/workflows/AppStore/Keychain.sh
@@ -8,7 +8,7 @@ security set-keychain-settings -lut 3600
 security list-keychains
 
 tuist signing decrypt
-security import "Tuist/Signing/dotori.p12" -k "$KEYCHAIN" -P "$CERTS_EXPORT_PWD" -A
+security import "Tuist/Signing/todaywhat.p12" -k "$KEYCHAIN" -P "$CERTS_EXPORT_PWD" -A
 security set-key-partition-list -S apple-tool:,apple: -s -k "" "$KEYCHAIN"
 
 mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles" 
