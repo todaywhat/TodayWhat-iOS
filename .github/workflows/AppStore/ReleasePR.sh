f2314dc61b6c7848fcaa2a870285c9405362277a
baegteun <baegteun@gmail.com>
2024-02-07 19:02:21 +0900

    :pencil2: :: baegteun -> baekteun

--- .github/workflows/AppStore/ReleasePR.sh
+++ .github/workflows/AppStore/ReleasePR.sh
@@ -3,4 +3,4 @@
 git add .
 git commit -m "📝 :: $VERSION released"
 git push -u origin release/$VERSION
-gh pr create --repo baegteun/TodayWhat-new --title "🔀 :: $VERSION Release" --body "$VERSION version released" --base "master" --head "release/$VERSION" --assignee @me --label "🌏 Deploy"
+gh pr create --repo baekteun/TodayWhat-new --title "🔀 :: $VERSION Release" --body "$VERSION version released" --base "master" --head "release/$VERSION" --assignee @me --label "🌏 Deploy"
--- .github/workflows/TestFlight/BuildNumberIncreasePR.sh
+++ .github/workflows/TestFlight/BuildNumberIncreasePR.sh
@@ -3,4 +3,4 @@
 git add .
 git commit -m "📝 :: build number increase"
 git push -u origin build-number-increase-for-testflight
-gh pr create --repo baegteun/TodayWhat-new --title "🔀 :: build number increase" --body "build number increased" --base "master" --head "build-number-increase-for-testflight" --assignee @me --label "🌏 Deploy"
+gh pr create --repo baekteun/TodayWhat-new --title "🔀 :: build number increase" --body "build number increased" --base "master" --head "build-number-increase-for-testflight" --assignee @me --label "🌏 Deploy"
