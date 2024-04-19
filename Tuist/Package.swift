57558a8a9bc0f2f7050c8eb25f86ac8c15077e20
baegteun <baegteun@gmail.com>
2024-04-19 23:38:41 +0900

    :recycle: :: [#129] Dependencies -> Package 의존성 관리 방법 migration

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,6 +1,24 @@
 // swift-tools-version:5.7
 import PackageDescription
 
+#if TUIST
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let packageSetting = PackageSettings(
+    productTypes: [
+        "GRDB": .framework
+    ],
+    baseSettings: .settings(
+        configurations: [
+            .debug(name: .dev),
+            .debug(name: .stage),
+            .release(name: .prod)
+        ]
+    )
+)
+#endif
+
 let package = Package(
     name: "TodayWhatPackage",
     dependencies: [
