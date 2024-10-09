e88adf20ddad7a264752d83ab01cabdf00437bf0
baegteun <baegteun@gmail.com>
2024-10-09 12:07:49 +0900

    :seedling: :: [#167] ci_post_clone 설정"

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,4 +1,8 @@
-git clone https://github.com/baekteun/TodayWhat-XCConfig.git
+git clone https://github.com/todaywhat/TodayWhat-Secret.git
+
 cd ../
+
+rm -rf XCConfig/
+
 mv ./ci_scripts/Todaywhat-xcconfig/XCConfig .
-mv ./ci_scripts/TodayWhat-XCConfig/GoogleService-Info.plist ./TodayWhat/GoogleService-Info.plist
+mv ./ci_scripts/TodayWhat-XCConfig/GoogleService-Info.plist ./Projects/App/iOS/Resources/GoogleService-Info.plist
