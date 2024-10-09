4b32edef48fce9eae88c732c760671aae19bb48f
baegteun <baegteun@gmail.com>
2024-10-09 12:35:02 +0900

    ci_post_clone 예외처리 제거

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,14 +6,7 @@ cd ../
 
 rm -rf XCConfig/
 
-if ! mv ./TodayWhat-Secret/XCConfig .; then
-    echo "❌ Failed to move XCConfig"
-    exit 1
-fi
-
-if ! mv ./TodayWhat-Secret/GoogleService-Info.plist ./Projects/App/iOS/Resources/GoogleService-Info.plist; then
-    echo "❌ Failed to move GoogleService-Info.plist"
-    exit 1
-fi
+mv ./TodayWhat-Secret/XCConfig .
+mv ./TodayWhat-Secret/GoogleService-Info.plist ./Projects/App/iOS/Resources/GoogleService-Info.plist
 
 make cd_generate
