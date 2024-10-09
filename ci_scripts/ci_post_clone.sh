ad7d20ceafe9b15cb29118e6abb85e0b319d0cd0
baegteun <baegteun@gmail.com>
2024-10-09 12:15:57 +0900

    :recycle: :: [#167] 에러 핸들링

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 git clone https://github.com/todaywhat/TodayWhat-Secret.git
 
 cd ../
 
 rm -rf XCConfig/
 
-mv ./ci_scripts/Todaywhat-xcconfig/XCConfig .
-mv ./ci_scripts/TodayWhat-XCConfig/GoogleService-Info.plist ./Projects/App/iOS/Resources/GoogleService-Info.plist
+if ! mv ./TodayWhat-Secret/XCConfig .; then
+    echo "❌ Failed to move XCConfig"
+    exit 1
+fi
+
+if ! mv ./TodayWhat-Secret/GoogleService-Info.plist ./Projects/App/iOS/Resources/GoogleService-Info.plist; then
+    echo "❌ Failed to move GoogleService-Info.plist"
+    exit 1
+fi
