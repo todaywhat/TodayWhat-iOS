6b0984417b4e9b442b3475ffe7fd4506cd27fd48
baegteun <baegteun@gmail.com>
2024-10-09 12:39:09 +0900

    :sparkles: :: ci_post_clone tuist

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -9,4 +9,10 @@ rm -rf XCConfig/
 mv ./TodayWhat-Secret/XCConfig .
 mv ./TodayWhat-Secret/GoogleService-Info.plist ./Projects/App/iOS/Resources/GoogleService-Info.plist
 
+curl https://mise.run | sh
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+eval "$(/opt/homebrew/bin/mise activate --shims bash)"
+
+mise install
+
 make cd_generate
