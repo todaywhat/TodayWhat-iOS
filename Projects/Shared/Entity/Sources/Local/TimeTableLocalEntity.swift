afc6585ed23ee1a0435f24992cdb4b93a5b53a11
baegteun <baegteun@gmail.com>
2025-12-07 21:24:18 +0900

    DateFormatter 캐싱

--- Projects/Shared/Entity/Sources/Local/TimeTableLocalEntity.swift
+++ Projects/Shared/Entity/Sources/Local/TimeTableLocalEntity.swift
@@ -46,17 +46,21 @@ public extension TimeTableLocalEntity {
         )
     }
 
+    private static let dateOnlyFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        dateFormatter.timeZone = .autoupdatingCurrent
+        dateFormatter.locale = .autoupdatingCurrent
+        return dateFormatter
+    }()
+
     func toTimeTables() -> [TimeTable] {
         let decoder = JSONDecoder()
         guard let items = try? decoder.decode([TimeTableItem].self, from: Data(timeTableData.utf8)) else {
             return []
         }
 
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyyMMdd"
-        dateFormatter.timeZone = .autoupdatingCurrent
-        dateFormatter.locale = .autoupdatingCurrent
-        let dateObject = dateFormatter.date(from: date)
+        let dateObject = Self.dateOnlyFormatter.date(from: date)
 
         return items.map { item in
             TimeTable(
